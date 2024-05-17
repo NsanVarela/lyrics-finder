1591467325b7c4e13d518fe129b861fbacfe237f
SANCHES_VARELA Nicolas <nicolas.sanches_varela@macsf.fr>
2024-05-17 17:31:10 +0200

    ajout api Genius - recherche detail artiste et description

--- src/App.js
+++ src/App.js
@@ -3,11 +3,39 @@ import Axios from 'axios';
 import { useState } from 'react';
 
 function App() {
-  const [artist, setArtist] = useState("");
-  const [title, setTitle] = useState("");
-  const [lyrics, setLyrics] = useState("");
+  // États pour stocker les résultats, l'artiste, le titre et les paroles
+  const [results, setResults] = useState([]);
+  const [artist, setArtist] = useState('');
+  const [title, setTitle] = useState('');
+  const [lyrics, setLyrics] = useState('');
+  const [description, setDescription] = useState('');
 
-  function searchLyrics() {
+
+
+const optionsDetails = {
+  method: 'GET',
+  url: 'https://genius-song-lyrics1.p.rapidapi.com/search/',
+  params: {
+    q: artist,
+    per_page: '10',
+    page: '1'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'e3afcba8d6msha838af3aed0bec3p1fdf8djsnc09bd9425cd3',
+    'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+  }
+};
+
+  const searchArtistDetails = () => {
+    Axios.request(optionsDetails).then(res => {
+      console.log('Details : ', res.data.hits);
+      setResults(res.data.hits);
+    }).catch(error => {
+      console.log('Error fetching data:', error);
+    });
+  }
+
+  const searchLyrics = () => {
     if (artist === "" || title === "") {
       return;
     }
@@ -18,10 +46,35 @@ function App() {
       })
   }
 
+  // Extraire l'ID de l'artiste principal du premier résultat
+  const artistId = results.length > 0 ? results[0].result.primary_artist.id : null;
+  
+  const optionsDescription = {
+    method: 'GET',
+    url: 'https://genius-song-lyrics1.p.rapidapi.com/artist/details/',
+    params: {id: artistId},
+    headers: {
+      'X-RapidAPI-Key': 'e3afcba8d6msha838af3aed0bec3p1fdf8djsnc09bd9425cd3',
+      'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+    }
+  };
+
+  const searchDescriptionArtist = () => {
+    if (artistId === null) {
+      return;
+    }
+    Axios.request(optionsDescription).then(res => {
+      console.log('Description : ', res.data.artist.description_preview);
+      setDescription(res.data.artist.description_preview)
+    }).catch(error => {
+      console.log('Error fetching data:', error);
+    });
+  };
+
+  
+
   return (
     <div className="App">
-      <h1>Lyrics Finder ???</h1>
-
       <input className="inp" type="text"
         placeholder='Artist name'
         onChange={(e) => { setArtist(e.target.value) }} />
@@ -30,12 +83,36 @@ function App() {
         onChange={(e) => { setTitle(e.target.value) }} />
       <button className="btn"
         onClick={() => searchLyrics()}>
-        ??? Search
+        Search Lyrics
+      </button>
+      <button className="btn"
+        onClick={() => searchArtistDetails()}>
+        Search Details
+      </button>
+      <button className="btn"
+        onClick={() => searchDescriptionArtist()}>
+        Search Description
       </button>
       <hr />
-  <pre>{lyrics}</pre>
+      <pre>{lyrics}</pre>
+      {artistId && (
+        <div>
+          <h3>{description}</h3>
+        </div>
+      )}
+      <ul>
+        {results.map((song, index) => (
+          <li key={index}>
+            <h2>{song.result.full_title}</h2>
+            <p>{song.result.artist_names}</p>
+            <img src={song.result.header_image_thumbnail_url} alt={song.result.full_title} />
+            <p>{song.result.release_date_for_display}</p>
+            <a href={song.result.url} target="_blank" rel="noopener noreferrer">Lyrics</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
